7afcab1ea053ac8d93fde546765993f3879c372c
raivis_rsn <raivis.rasnacs@edu.jekabpils.lv>
2024-02-09 20:50:37 +0200

    v4

--- test_main.py
+++ test_main.py
@@ -1,12 +1,6 @@
 import main
 
 def test_summa():
-    if main.summa(3, 4) != 7:
-        return "Test failed!"
-    if main.summa(6, 2) != 8:
-        return "Test failed!"
-    if main.summa(1, 1) != 7:
-        return "Test failed!"
-    return "Test passed!"
-
-print(test_summa())
+    assert main.summa(2, 5) == 7
+    assert main.summa(1, 2) == 3
+    assert main.summa(6, 5) == 11
